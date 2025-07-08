2c6bc20489bbce50534c35d46ef95cd868bd84b7
= <=>
2025-07-07 23:54:11 -0400

    WIP Adding custom implementation of SHA512

--- src/algorithms/hmac_sha_512.rs
+++ src/algorithms/hmac_sha_512.rs
@@ -1,4 +1,31 @@
+use std::{io::Read, slice::Chunks};
+
 use sha2::{Sha512, Digest};
+
+pub fn sha512(mut message: String) -> Vec<String> {
+    const H0: u128 = 0x6a09e667f3bcc908;
+    const H1: u128 = 0xbb67ae8584caa73b;
+    const H2: u128 = 0x3c6ef372fe94f82b;
+    const H3: u128 = 0xa54ff53a5f1d36f1;
+    const H4: u128 = 0x510e527fade682d1;
+    const H5: u128 = 0x9b05688c2b3e6c1f;
+    const H6: u128 = 0x1f83d9abfb41bd6b;
+    const H7: u128 = 0x5be0cd19137e2179;
+    let mut bytes = message.as_bytes().to_vec();
+    let mut words: Vec<String> = Vec::new();
+    bytes.push(0x80);
+    while bytes.len() % 127 != 0  {
+        bytes.push(0);
+    }
+    let length = message.len() as u8;
+    bytes.push(&length * 8);
+    
+    let chunks = bytes.chunks(8);
+    for chunk in chunks {
+        words.push(hex::encode(chunk));
+    }
+    words
+}
 
 pub fn hmac_sha512(mut key: Vec<u8>, message: &'static [u8]) -> String {
     let mut outer_pad: Vec<u8> = Vec::new();
--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,15 @@ pub mod algorithms;
 
 #[cfg(test)]
 mod tests {
-    use crate::algorithms::{hmac_sha_256::hmac_sha256, hmac_sha_512::hmac_sha512, };
+    use crate::algorithms::{hmac_sha_256::hmac_sha256, hmac_sha_512::{hmac_sha512, sha512}, };
 
     #[test]
     fn it_works() {
+        println!("{}", hex::encode("3732313031313038313038313131303030"));
         let sha256 = hmac_sha256(b"Jefe".to_vec(), b"what do ya want for nothing?");
-        let sha512 = hmac_sha512(b"Jefe".to_vec(), b"heaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-
+        let sha5123 = hmac_sha512(b"Jefe".to_vec(), b"heaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        println!("{:?}", sha512("Hello".to_string()));
         assert_eq!(sha256, "7112d130efb0ed1417a6c9f80ea18d1b51c76a173236c29cda4a7e87b88413b2");
-        assert_eq!(sha512, "99d12091f2112502c9062a3a072c463cf15240035acac6db13bba2556e36a3a902ae610401f7c2552ca82d624b880ec8d8adba7ca037aeb68448f09f5af5c92d");
+        assert_eq!(sha5123, "99d12091f2112502c9062a3a072c463cf15240035acac6db13bba2556e36a3a902ae610401f7c2552ca82d624b880ec8d8adba7ca037aeb68448f09f5af5c92d");
     }
 }
