8f72e54cc9f165fa618bb3a3f6f3fa0c4412d829
Kayla Bevans <joshprince230@yahoo.com>
2026-03-23 23:22:00 -0400

    Added sha256 hash function helper

--- src/algorithms/hmac_sha_256.rs
+++ src/algorithms/hmac_sha_256.rs
@@ -1,61 +1,62 @@
-use sha2::{Sha256, Digest};
+    use sha2::{Sha256, Digest};
 
-pub fn hmac_sha256(mut key: Vec<u8>, message: Vec<u8>) -> Vec<u8> {
-    // The outter pad is going to store the padded key,
-    // with the hashed bytes for the inner pad, concatenated to the message bytes
-    let mut outter_pad: Vec<u8> = Vec::new();
-    let mut inner_pad: Vec<u8> = Vec::new();
-
-    // Create a SHA256 hasher for the inner pad
-    let mut inner_pad_hasher = Sha256::new();
-
-    // Create a SHA256 hasher for the outter pad
-    let mut outter_pad_hasher = Sha256::new();
-
-    if key.len() > 64 {
+    pub fn sha256(data: Vec<u8>) -> Vec<u8> {
         let mut hasher = Sha256::new();
-        hasher.update(key);
-        key = hasher.finalize().to_vec();
+        hasher.update(data);
+        
+        let inner_hash = hasher
+            .finalize()
+            .to_vec();
+
+        inner_hash
     }
 
-    let mut key_appended_message = key.clone();
-    
-    // Make sure the key block is 512 bytes by padding it out with 0s
-    while key_appended_message.len() % 64 != 0 ||
-        key_appended_message.len() == 0 {
-        key_appended_message.push(0);
+    pub fn hmac_sha256(mut key: Vec<u8>, message: Vec<u8>) -> Vec<u8> {
+        // The outter pad is going to store the padded key,
+        // with the hashed bytes for the inner pad, concatenated to the message bytes
+        let mut outter_pad: Vec<u8> = Vec::new();
+        let mut inner_pad: Vec<u8> = Vec::new();
+
+        if key.len() > 64 {
+            let mut hasher = Sha256::new();
+            hasher.update(key);
+            key = hasher.finalize().to_vec();
+        }
+
+        let mut key_appended_message = key.clone();
+        
+        // Make sure the key block is 512 bytes by padding it out with 0s
+        while key_appended_message.len() % 64 != 0 ||
+            key_appended_message.len() == 0 {
+            key_appended_message.push(0);
+        }
+
+        // xor each byte in the key with 0x36, push the result to the inner pad vector
+        for byte in key_appended_message.clone() {
+            inner_pad.push(byte ^ 0x36);
+        }
+
+        // Finally concatenate the message bytes to the ipad
+        for byte in message {
+            inner_pad.push(byte);
+        }
+
+        // hash the inner_pad bytes
+        let inner_hash = sha256(inner_pad);
+
+        // Push the key to the outter pad
+        for byte in key_appended_message {
+            outter_pad.push(byte ^ 0x5C);
+        }
+
+        // Append the hashed ipad and message bytes to the outter pad bytes
+        for byte in inner_hash {
+            outter_pad.push(byte);
+        }
+
+        // Hash the outter final data
+        let outter_hash_bytes = sha256(outter_pad);
+
+        // Return the hashed bytes as a hex string
+        outter_hash_bytes.to_vec()
     }
-
-    // xor each byte in the key with 0x36, push the result to the inner pad vector
-    for byte in key_appended_message.clone() {
-        inner_pad.push(byte ^ 0x36);
-    }
-
-    // Finally concatenate the message bytes to the ipad
-    for byte in message {
-        inner_pad.push(byte);
-    }
-
-    // hash the inner_pad bytes
-    inner_pad_hasher.update(inner_pad);
-    
-    let inner_hash = inner_pad_hasher.finalize();
-
-    // Push the key to the outter pad
-    for byte in key_appended_message {
-        outter_pad.push(byte ^ 0x5C);
-    }
-
-    // Append the hashed ipad and message bytes to the outter pad bytes
-    for byte in inner_hash {
-        outter_pad.push(byte);
-    }
-
-    // Hash the outter final data
-    outter_pad_hasher.update(outter_pad);
-    
-    let outter_hash_bytes = outter_pad_hasher.finalize();
-    
-    // Return the hashed bytes as a hex string
-    outter_hash_bytes.to_vec()
-}
